d7f066908f23d5f02d1352322411ae82eecad0f4
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-02-21 23:44:08 +0800

    preparation for learning to create a new programming language

---
+++ programming/index.js
@@ -0,0 +1,65 @@
+function parseExpression(program) {
+  program = skipSpace(program);
+  var match, expr;
+  if ((match = /^"([^"]*)"/.exec(program)))
+    expr = { type: "value", value: match[1] };
+  else if ((match = /^\d+\b/.exec(program)))
+    expr = { type: "value", value: Number(match[0]) };
+  else if ((match = /^[^\s(),"]+/.exec(program)))
+    expr = { type: "word", name: match[0] };
+  else throw new SyntaxError("Unexpected syntax: " + program);
+  return parseApply(expr, program.slice(match[0].length));
+}
+
+function skipSpace(string) {
+  var first = string.search(/\S/);
+  if (first == -1) return "";
+  return string.slice(first);
+}
+
+function parseApply(expr, program) {
+  program = skipSpace(program);
+  if (program[0] != "(") return { expr: expr, rest: program };
+  program = skipSpace(program.slice(1));
+  expr = { type: "apply", operator: expr, args: [] };
+  while (program[0] != ")") {
+    var arg = parseExpression(program);
+    expr.args.push(arg.expr);
+    program = skipSpace(arg.rest);
+    if (program[0] == ",") program = skipSpace(program.slice(1));
+    else if (program[0] != ")") throw new SyntaxError("Expected ',' or ')'");
+  }
+  return parseApply(expr, program.slice(1));
+}
+
+function parse(program) {
+  var result = parseExpression(program);
+  if (skipSpace(result.rest).length > 0)
+    throw new SyntaxError("Unexpected text after program");
+  return result.expr;
+}
+console.log(parse("+(a, 10)"));
+
+// The Evaluator
+function evaluate(expr, env) {
+  switch (expr.type) {
+    case "value":
+      return expr.value;
+    case "word":
+      if (expr.name in env) return env[expr.name];
+      else throw new ReferenceError("Undefined variable: " + expr.name);
+    case "apply":
+      if (expr.operator.type == "word" && expr.operator.name in specialForms)
+        return specialForms[expr.operator.name](expr.args, env);
+      var op = evaluate(expr.operator, env);
+      if (typeof op != "function")
+        throw new TypeError("Applying a non-function.");
+      return op.apply(
+        null,
+        expr.args.map(function(arg) {
+          return evaluate(arg, env);
+        })
+      );
+  }
+}
+var specialForms = Object.create(null);
