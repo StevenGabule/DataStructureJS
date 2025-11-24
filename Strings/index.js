417a0eb180772fbbabac7fa8e14096012f051e0e
John Paul Gabule <lucas.gabule@gmail.com>
2025-11-24 20:50:04 +0800

    resolve palindrome - easy probl

--- Strings/index.js
+++ Strings/index.js
@@ -1,18 +1,18 @@
 
 // check if string contains substring
 const url = 'https://api.github.com/users/stevengabule'
-console.log(url.includes('github'))
+console.log(url.includes('github')); // true
 
 // iterate over a string
 var str = 'hello world'
-for(let i = 0; i < str.length; i++) {
-    console.log(str.charAt(i), i)
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i], i)
 }
 
-for(let s in str) console.log(str.charAt(s)) 
-    
+for (let s in str) console.log(str[s])
+
 var newStr = str.split('');
-for(let [i, n] of newStr.entries()) console.log(n)
+for (let [i, n] of newStr.entries()) console.log({ i, n })
 
 // capitalize First word of string
 function capitalize(str) {
@@ -22,7 +22,7 @@ function capitalize(str) {
 var result = capitalize('hello world, of programming')
 console.log(result)
 
-var capitalize_ = ([first,...rest]) => `${first.toUpperCase()}${rest.join('')}`
+var capitalize_ = ([first, ...rest]) => `${first.toUpperCase()}${rest.join('')}`
 console.log(capitalize_('hello world'))
 
 
@@ -31,3 +31,11 @@ function capitalizeWords(str) {
     return str.split(' ').map(word => `${word[0].toUpperCase()}${word.slice(1)}`).join(' ')
 }
 console.log(capitalizeWords('hello world of programming in good way'))
+
+function cptStatement(word) {
+    return word
+        .split(' ')
+        .map(([first, ...rest]) => `${first.toUpperCase()}${rest.join('')}`)
+        .join(' ')
+}
+console.log(cptStatement('hello world of programming in good way'))
--- stacks/index.js
+++ stacks/index.js
@@ -47,18 +47,18 @@ s.push("Clayton");
 console.log(s.peek());
 */
 
-function mulBase(num, base) {
-    let s = new Stack();
-    do {
-        s.push(num % base);
-        num = Math.floor(num /= base);
-    } while (num > 0);
-    let converted = "";
-    while (s.length() > 0) {
-        converted += s.pop();
-    }
-    return converted;
-}
+// function mulBase(num, base) {
+//     let s = new Stack();
+//     do {
+//         s.push(num % base);
+//         num = Math.floor(num /= base);
+//     } while (num > 0);
+//     let converted = "";
+//     while (s.length() > 0) {
+//         converted += s.pop();
+//     }
+//     return converted;
+// }
 
 /*
 let num = 32, base = 2;
@@ -71,49 +71,79 @@ console.log(num + " converted to base " + base + " is " + newNum1);
 */
 
 /* Determining if a string is a palindrome */
-function isPalindrome(word) {
-    let s = new Stack();
-    let _ = word.length;
-    for (let i = 0; i < _; ++i) {
-        s.push(word[i]);
+// function isPalindrome(word) {
+//     let s = new Stack();
+//     let _ = word.length;
+//     for (let i = 0; i < _; ++i) {
+//         s.push(word[i]);
+//     }
+//     let rword = "";
+//     while (s.length() > 0) {
+//         rword += s.pop();
+//     }
+//     return word === rword;
+// }
+
+// let word = 'hello';
+// if (isPalindrome(word)) {
+//     console.log(word + ' is a palindrome');
+// } else {
+//     console.log(word + ' is not palindrome');
+// }
+
+// // word = 'racecar';
+// word = '1001';
+// if (isPalindrome(word)) {
+//     console.log(word + ' is a palindrome');
+// } else {
+//     console.log(word + ' is not palindrome');
+// }
+
+
+// function checkPalindrome(text) {
+// Issues with Current Code
+// Extra memory allocation – split(''), reverse(), and join('') each create new arrays/strings
+// Loose equality – using == instead of ===
+// More complex than needed – slicing and reversing half the string is indirect
+//     const textLength = text.length;
+//     const halfLength = textLength / 2;
+//     const halfString = text.slice(0, Math.floor(halfLength));
+//     const secondHalfString =
+//         text
+//             .slice(Math.round(halfLength), textLength)
+//             .split('')
+//             .reverse()
+//             .join('');
+//     return halfString == secondHalfString;
+// }
+
+// Simple two-pointer approach (most efficient):
+// This is O(n) time and O(1) space – no extra strings or arrays created, 
+// and it exits early on the first mismatch.
+function checkPalindrome(text) {
+    const cleanString = text.toLowerCase().replace(/^[a-z0-9]/g, ''); // ignoring case and non-alphanumeric characters
+    let pointLeft = 0;
+    let pointRight = cleanString.length - 1;
+
+    while (pointLeft < pointRight) {
+        if (text[pointLeft] !== text[pointRight]) {
+            return false;
+        }
+        pointLeft++;
+        pointRight--;
     }
-    let rword = "";
-    while (s.length() > 0) {
-        rword += s.pop();
-    }
-    return word === rword;
+
+    return true;
 }
 
-let word = 'hello';
-if (isPalindrome(word)) {
-    console.log(word +  ' is a palindrome');
-} else {
-    console.log(word +  ' is not palindrome');
-}
+// Concise one-liner (if brevity matters more than performance):
+// function checkPalindrome(text) {
+//     return text === [...text].reverse().join('')
+// }
 
-// word = 'racecar';
-word = '1001';
-if (isPalindrome(word)) {
-    console.log(word +  ' is a palindrome');
-} else {
-    console.log(word +  ' is not palindrome');
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(checkPalindrome('1000001')); // t
+console.log(checkPalindrome('racecar')); // t
+console.log(checkPalindrome('raceecar'));// t
+console.log(checkPalindrome('111111')); // t
+console.log(checkPalindrome('121131')); // f
 
